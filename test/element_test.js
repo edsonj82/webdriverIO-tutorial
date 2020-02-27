f5d9edd710214b2437a707b67bd5fbb17f3d28e9
Edson <edsonj82@outlook.com>
2020-02-27 20:29:16 -0300

    Clean Repository

--- test/element_test.js
+++
@@ -1,7 +0,0 @@
-describe('Interacting with elemtns', function (params) {
-    it('Get text for element',() => {
-        browser.url('/')
-        let text = $("#page-footer").getText()
-        console.log(text)
-    })
-})
